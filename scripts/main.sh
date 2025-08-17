294f768cf9be634ba2af761b280eb97b7e91a35a
pedro-git-projects <pedro.coding.contact@gmail.com>
2025-08-17 16:45:15 -0300

    chore: removing shellcheck comments

--- scripts/main.sh
+++ scripts/main.sh
@@ -4,23 +4,15 @@ IFS=$'\n\t'
 
 SCRIPT_DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
 
-# shellcheck source=./config.sh
 source "$SCRIPT_DIR/config.sh"
-# shellcheck source=./lib/log.sh
 source "$SCRIPT_DIR/lib/log.sh"
-# shellcheck source=./lib/run_once.sh
 source "$SCRIPT_DIR/lib/run_once.sh"
-# shellcheck source=./lib/x11.sh
 source "$SCRIPT_DIR/lib/x11.sh"
 
 # modules
-# shellcheck source=./modules/monitors.sh
 source "$SCRIPT_DIR/modules/monitors.sh"
-# shellcheck source=./modules/compositor.sh
 source "$SCRIPT_DIR/modules/compositor.sh"
-# shellcheck source=./modules/wallpaper.sh
 source "$SCRIPT_DIR/modules/wallpaper.sh"
-# shellcheck source=./modules/services.sh
 source "$SCRIPT_DIR/modules/services.sh"
 
 require_bins xrandr pgrep feh
