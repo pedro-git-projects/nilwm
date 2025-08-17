32f3ca08ae794b24007f5596228524ac58b748f9
pedro-git-projects <pedro.coding.contact@gmail.com>
2025-08-17 19:08:22 -0300

    feat: modding datetime to receive an optional timezone argument

--- slstatus/components/datetime.c
+++ slstatus/components/datetime.c
@@ -1,20 +1,48 @@
-/* See LICENSE file for copyright and license details. */
+// patched to be have an optional TZ parameter
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 #include "../slstatus.h"
 #include "../util.h"
 
-const char *
-datetime(const char *fmt)
-{
-	time_t t;
+const char *datetime(const char *fmt) {
+  time_t t;
+  const char *real_fmt = fmt;
+  char tzbuf[64];
+  const char *oldtz = getenv("TZ");
 
-	t = time(NULL);
-	if (!strftime(buf, sizeof(buf), fmt, localtime(&t))) {
-		warn("strftime: Result string exceeds buffer size");
-		return NULL;
-	}
+  t = time(NULL);
 
-	return buf;
+  if (fmt && strncmp(fmt, "TZ=", 3) == 0) {
+    const char *tz = fmt + 3;
+    const char *space = strchr(tz, ' ');
+    if (space) {
+      size_t len = space - tz;
+      if (len < sizeof(tzbuf)) {
+        memcpy(tzbuf, tz, len);
+        tzbuf[len] = '\0';
+        setenv("TZ", tzbuf, 1);
+        tzset();
+        real_fmt = space + 1;
+      }
+    }
+  }
+
+  if (!strftime(buf, sizeof(buf), real_fmt, localtime(&t))) {
+    warn("strftime: Result string exceeds buffer size");
+    buf[0] = '\0';
+  }
+
+  /* restore old TZ (or unset if none existed) */
+  if (fmt && strncmp(fmt, "TZ=", 3) == 0) {
+    if (oldtz)
+      setenv("TZ", oldtz, 1);
+    else
+      unsetenv("TZ");
+    tzset();
+  }
+
+  return buf;
 }
